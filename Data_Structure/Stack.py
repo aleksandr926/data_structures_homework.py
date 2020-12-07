5cefc2941ef29bb7d787d1e959079ef33d12b1e1
aleksandr926 <72929711+aleksandr926@users.noreply.github.com>
2020-12-07 16:52:36 +0400

    Update Stack.py

--- Data_Structure/Stack.py
+++ Data_Structure/Stack.py
@@ -1,48 +1,19 @@
-# class Node:
-#    def __init__(self):
-#       self.node = []
-#
-#    def update(self, key):
-#       i_f = False
-#       for i, k in enumerate(self.node):
-#          if key == k:
-#             self.node[i] = key
-#             i_f = True
-#             break
-#       if not i_f:
-#          self.node.append(key)
-#
-#    def get(self, key):
-#       for k in self.node:
-#          if k == key:
-#             return True
-#       return False
-#
-#    def remove(self, key):
-#       for i, k in enumerate(self.node):
-#          if key == k:
-#             del self.node[i]
-# class HashSet:
-#    def __init__(self):
-#       self.space_key = 1234
-#       self.hash_map = [Node() for i in range(self.space_key)]
-#
-#    def cont(self, key):
-#       hash_key = key % self.space_key
-#       return self.hash_map[hash_key].get(key)
-#
-#    def add(self, key):
-#       hash_key = key % self.space_key
-#       self.hash_map[hash_key].update(key)
-#
-#    def remove(self, key):
-#       hash_key = key % self.space_key
-#       self.hash_map[hash_key].remove(key)
-#
-# y = Node()
-# print(y.update())
-# print(y.get())
-# x = HashSet()
-# print(x.cont())
-# print(x.add())
-# print(x.remove())
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def add(self, item):
+        self.items.append(item)             
+
+    def remove(self):
+        return self.items.pop()
+    
+    def is_empty(self):
+        if self.items == []:
+            return 'No one in the queue'
+        else:
+            return 'Busy'
+    
+    def last(self):
+        if not self.is_empty():
+            return self.items[-1]
